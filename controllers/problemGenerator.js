aec179c8492336432d529534e868b56f6693c8b9
SorenErvik <ervik.soren@gmail.com>
2024-01-10 11:23:28 -0600

    Added import/export modules for functionality of game

---
+++ controllers/problemGenerator.js
@@ -0,0 +1,41 @@
+/*
+This module is responsible for generating the math problems for the user to solve.
+*/
+
+
+let a = Math.floor(Math.random() * 100) + 1; // Determining the first number
+let b = Math.floor(Math.random() * 100) + 1; // Determining the second number
+let operator; // Declare the operator variable outside the function
+
+function determineOperator() {
+    let num = Math.random();
+    if (num < 0.26) {
+      operator = "+";
+    } else if (num < 0.51 && num > 0.25) {
+      operator = "-";
+    } else if (num < 0.76 && num > 0.5) {
+      operator = "*";
+    } else if (num > 0.75) {
+      operator = "/";
+    }
+  }
+
+  determineOperator();
+
+let question = `What is ${a} ${operator} ${b}? Round down to the nearest whole number.`;
+
+function calculateAnswer(a, b, operator) {
+    switch (operator) {
+      case "+":
+        return Math.floor(a + b);
+      case "-":
+        return Math.floor(a - b);
+      case "*":
+        return Math.floor(a * b);
+      case "/":
+        // Ensure b is not 0 to avoid division by zero
+        return Math.floor(a / b);
+      default:
+        return "Invalid operator";
+    }
+  }
---
+++ controllers/userInput.js
@@ -0,0 +1,5 @@
+/*
+This module is responsible for grabbing the user's input.
+*/
+
+let userInput = document.querySelector('input').value;
