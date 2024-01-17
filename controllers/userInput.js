4389082b5d8717de503705f60073291a03562dd4
SorenErvik <ervik.soren@gmail.com>
2024-01-17 10:15:40 -0600

    Added clarifying comments to userInput.js

--- controllers/userInput.js
+++ controllers/userInput.js
@@ -1,17 +1,35 @@
+/*
+ This module is responsible for handling user input.
+*/
+
+// This function handles the user input
 const userInputModule = () => {
+
+  // Declaring userInputElement and submitButton
   let userInputElement = document.getElementById('userInput');
   let submitButton = document.getElementById('submitButton');
 
+  // Creating the handleButtonClick function
   function handleButtonClick() {
+
+    // This returns a promise that will be resolved
     return new Promise((resolve, reject) => {
+
+      // Create the click handler for the user input
       const clickHandler = () => {
+
+        // Declaring userInput and giving it the appropriate value
         let userInput = userInputElement.value;
+
+        // If statement to make sure that the user input is valid
         if (isValidInput(userInput)) {
           // Parse the user input as a number
           userInput = parseFloat(userInput);
           
           console.log('User Input:', userInput);
           resolve(userInput);
+
+          // Handle errors
         } else {
           console.error('Invalid Input. Please enter a valid response.');
           reject('Invalid Input');
@@ -26,14 +44,17 @@ const userInputModule = () => {
     });
   }
 
+  // Make sure the input is valid
   function isValidInput(input) {
     // Check if the input is a non-empty string
     return typeof input === 'string' && input.trim() !== '';
   }
 
+  // Returning handleButtonClick
   return {
     handleButtonClick,
   };
 };
 
+// Export the whole moduele
 export default userInputModule();
